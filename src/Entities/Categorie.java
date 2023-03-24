44cbe1bd2b22439f90be98bcea74b1bb918215e9
Lucky12348 <113895107+Lucky12348@users.noreply.github.com>
2023-03-24 09:22:21 +0100

    Création du test JAVA

--- src/Entities/Categorie.java
+++ src/Entities/Categorie.java
@@ -9,5 +9,56 @@ package Entities;
  * @author Lucky1234
  */
 public class Categorie {
+    private int codeCategorie;
+    private String libelle;
+    private double prix;
     
+    public Categorie (int uncodeCategorie,String unlibelle,double unprix)
+    {
+        codeCategorie = uncodeCategorie;
+        libelle = unlibelle;
+        prix = unprix;
+    }
+
+    /**
+     * @return the codeCategorie
+     */
+    public int getCodeCategorie() {
+        return codeCategorie;
+    }
+
+    /**
+     * @param codeCategorie the codeCategorie to set
+     */
+    public void setCodeCategorie(int codeCategorie) {
+        this.codeCategorie = codeCategorie;
+    }
+
+    /**
+     * @return the libelle
+     */
+    public String getLibelle() {
+        return libelle;
+    }
+
+    /**
+     * @param libelle the libelle to set
+     */
+    public void setLibelle(String libelle) {
+        this.libelle = libelle;
+    }
+
+    /**
+     * @return the prix
+     */
+    public double getPrix() {
+        return prix;
+    }
+
+    /**
+     * @param prix the prix to set
+     */
+    public void setPrix(double prix) {
+        this.prix = prix;
+    }
 }
